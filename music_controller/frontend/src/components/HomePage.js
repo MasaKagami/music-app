33a3ad10ef2c55285f80d13d7b71959136bdcd58
MasaKagami <97225192+MasaKagami@users.noreply.github.com>
2024-06-17 08:52:08 -0400

    yuh

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -1,24 +1,22 @@
 import React, { Component } from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
-import { BrowserRouter as Router, Switch, Router, Link, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// don't need to render since app.js is already rendered
+export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-export default class HomePage extends Component{
-    constructor(props) {
-        super(props); //parent constructor
-    }
-
-    render() {
-        return 
-        <Router>
-            <Switch>
-                <Route exact path='/'><p>This is the home page</p></Route>
-                <Route path='/join' component = {RoomJoinPage}></Route> 
-                <Route path='/create' component = {CreateRoomPage}></Route> { /* have to add the path to both django and react */}
-
-            </Switch>
-        </Router>;
-    }
+  render() {
+    return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<p>This is the home page</p>} />
+          <Route path="/join" element={<RoomJoinPage />} />
+          <Route path="/create" element={<CreateRoomPage />} />
+        </Routes>
+      </Router>
+    );
+  }
 }
